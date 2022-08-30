784cc17cc3832728da4094192bef4d0c55f766e2
Aayush <106085120+AyushSharma64047@users.noreply.github.com>
2022-08-30 12:33:15 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,18 +4,18 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 7588344))
-    API_HASH = os.environ.get("API_HASH", "6ade175bcf8ea4981e3153ef48fe07f5")
+    API_ID = int(os.environ.get("API_ID", 12336818))
+    API_HASH = os.environ.get("API_HASH", "de4c34807c8963ba9418e01c7cc15c4c")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "MdiskSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL",)
     ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
 
-🤖 My Name: <a href='https://t.me/FilmyFather_BotList'>Mdisk Search Robot</a>
+🤖 My Name: <a href='https://t.me/Mdisk_Links_Sender_Bot>Mdisk_Links_Sender_Bot</a>
 
 📝 Language : <a href='https://www.python.org'> Python V3</a>
 
@@ -23,10 +23,10 @@ class Config(object):
 
 📡 Server: <a href='https://heroku.com'>Heroku</a>
 
-👨‍💻 Created By: <a href='https://t.me/Yuvi_4502'>ʏᴜᴠʀᴀᴊ</a></b>
+👨‍💻 Created By: <a href='https://t.me/Z_Harbour_bot'>ʏᴜᴠʀᴀᴊ</a></b>
 """
 
-    ABOUT_HELP_TEXT = """<b>👨‍💻 Developer : <a href='https://t.me/Yuvi_4502'>ʏᴜᴠʀᴀᴊ</a>
+    ABOUT_HELP_TEXT = """<b>👨‍💻 Developer : <a href='https://t.me/Z_Harbour_bot'>ʏᴜᴠʀᴀᴊ</a>
 
 If You Want Your Own Bot Like This Then You Can Contact Our Developer.</b>
 """
@@ -38,7 +38,7 @@ I'm Mdisk Search Robot.🤖</a>
 
 I Can Search!🔍 What You Want?😜
 
-<a>Made With ❤ By @Yuvi_4502</a></b>
+<a>Made With ❤ By @Z_Harbour_bot</a></b>
 """
 
 
