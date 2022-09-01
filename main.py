311d0d2d7b134863a4e92694102de0730b8025b7
Aayush <106085120+AyushSharma64047@users.noreply.github.com>
2022-09-01 08:05:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def help_handler(_, event: Message):
 async def inline_handlers(_, event: Message):
     if event.text == '/start':
         return
-    answers = f'**📂 Results For ➠ {event.text} \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n➠ Please Type Correct Spelling And In CAPITAL LETTERS.✍️\n➠ Add Year For Better Result.🗓️\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
+    answers = f'**📂 Results For ➠ {event.text} \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n➠ Fcuk What'Re You Doing!Try Typing The Query in Correct Spelling,after that in CAPITAL LETTERS.✍️\n➠ Add Year For Better Result.🗓️For eg. Game Of Thrones Season 1 (2012),Marvel(20xx),MARVEL(20xx)😊...\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
     async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.text):
         if message.text:
             thumb = None
@@ -82,10 +82,11 @@ async def button(bot, cmd: CallbackQuery):
 				[
 					[
 						InlineKeyboardButton("Our Channel", url="https://t.me/z_harbour"),
-						InlineKeyboardButton("Our Group", url="https://t.me/blackest_harbour")
+						InlineKeyboardButton("Our Group", url="https://t.me/blackest_harbour"),
+                                                InlineKeyboardButton("About", callback_data="About_msg")
 					],
 					[
-						InlineKeyboardButton("Developer", url="https://t.me/sigma_male007"),
+						InlineKeyboardButton("Developer", url="https://t.me/Z_Harbour_bot"),
 						InlineKeyboardButton("Home", callback_data="gohome")
 					]
 				]
